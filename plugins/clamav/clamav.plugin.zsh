877b84d4f96c30dbb5d90792f694712fc576cd74
Shawn Webb <swebb@sourcefire.com>
2013-06-05 15:52:11 -0400

    Add more clamav stuff

--- plugins/clamav/clamav.plugin.zsh
+++ plugins/clamav/clamav.plugin.zsh
@@ -7,3 +7,19 @@ alias clamscan='$(echo ${clam}/clamscan/clamscan)'
 alias clamd='$(echo ${clam}/clamd/clamd)'
 alias clamdcscan='$(echo ${clam}/clamdscan/clamdscan)'
 alias clamdtop='$(echo ${clam}/clamdtop/clamdtop)'
+alias freshclam='$(echo ${clam}/freshclam/freshclam)'
+alias dbtool='$(echo ${clam}/dbtool/dbtool)'
+
+function buildclam() {
+    (
+        cd $clam
+
+        if [ -f config.status ]; then
+            gmake clean distclean
+        fi
+        autoreconf -fi && \
+        CC=clang CFLAGS="-g -O0" ./configure --enable-debug --disable-silent-rules --with-dbdir=/data/clamav/db --disable-clamav && \
+        gmake -j7 && \
+        gmake check
+    ) 2>&1 | tee /tmp/build.log
+}
