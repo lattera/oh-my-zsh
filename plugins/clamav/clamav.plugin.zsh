5174e07cd785499fff89cee9bad2a528839d1467
Shawn Webb <swebb@sourcefire.com>
2013-09-09 10:44:01 -0400

    Add valgrind function for clamav

--- plugins/clamav/clamav.plugin.zsh
+++ plugins/clamav/clamav.plugin.zsh
@@ -10,6 +10,7 @@ alias clamdscan='$(echo ${clam}/clamdscan/clamdscan) --config=${clamconfig}'
 alias clamdtop='$(echo ${clam}/clamdtop/clamdtop) --config=${clamconfig}'
 alias freshclam='$(echo ${clam}/freshclam/freshclam) --config=${clamconfig}'
 alias dbtool='$(echo ${clam}/dbtool/dbtool) --config=${clamconfig}'
+alias clamconf='$(echo ${clam}/clamconf/clamconf) --config=${clamconfig}'
 
 function buildclam() {
     (
@@ -19,8 +20,23 @@ function buildclam() {
             gmake clean distclean
         fi
         autoreconf -fi && \
-        CC=clang LDFLAGS="-v" CFLAGS="-g -O0" ./configure --enable-debug --disable-silent-rules --with-dbdir=/data/clamav/db --disable-clamav && \
-        LDFLAGS="-v" gmake -j7 && \
+        CC=clang LDFLAGS="-v" CFLAGS="-g -O0" ./configure --enable-debug --disable-silent-rules --with-dbdir=/data/clamav/db --disable-clamav --prefix=/data/clamav/install --enable-milter && \
+        gmake -j7 && \
         gmake check
     ) 2>&1 | tee /tmp/build.log
+}
+
+function vg() {
+    logfile=$(mktemp)
+
+    echo "[*] Logging to ${logfile}"
+
+    LD_LIBRARY_PATH=$clam/libclamav/.libs valgrind \
+        --verbose \
+        --track-fds=yes \
+        --log-file=${logfile} \
+        --read-var-info=yes \
+        --leak-check=full \
+        --track-origins=yes \
+        $*
 }
