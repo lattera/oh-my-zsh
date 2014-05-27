e070e4ff614e44d7b2e638d4d0ed897b21aa367e
Shawn Webb <swebb@sourcefire.com>
2014-05-27 12:43:41 -0400

    More enhancements

--- plugins/clamav/clamav.plugin.zsh
+++ plugins/clamav/clamav.plugin.zsh
@@ -1,15 +1,18 @@
 if [ "${clam}" = "" ]; then
     clam=${HOME}/clamav/clamav-devel
-    clamconfig=/data/clamav/conf/clamav.conf
     clamcc="clang"
+    clambase="/data/clamav"
+    conf_freshclam="freshclam.conf"
+    conf_clamd="clamd.conf"
+    dbdir="0.98"
 fi
 
 alias sigtool='$(echo ${clam}/sigtool/sigtool)'
 alias clamscan='$(echo ${clam}/clamscan/clamscan)'
-alias clamd='$(echo ${clam}/clamd/clamd)'
-alias clamdscan='$(echo ${clam}/clamdscan/clamdscan)'
-alias clamdtop='$(echo ${clam}/clamdtop/clamdtop)'
-alias freshclam='$(echo ${clam}/freshclam/freshclam)'
+alias clamd='$(echo ${clam}/clamd/clamd) --config-file=${clambase}/conf/${conf_clamd}'
+alias clamdscan='$(echo ${clam}/clamdscan/clamdscan) --config-file=${clambase}/conf/${conf_clamd}'
+alias clamdtop='$(echo ${clam}/clamdtop/clamdtop) --config-file=${clambase}/conf/${conf_clamd}'
+alias freshclam='$(echo ${clam}/freshclam/freshclam) --config-file=${clambase}/conf/${conf_freshclam}'
 alias dbtool='$(echo ${clam}/dbtool/dbtool)'
 alias clamconf='$(echo ${clam}/clamconf/clamconf)'
 
@@ -34,7 +37,7 @@ function buildclam() {
         if [ -f config.status ]; then
             ${make} clean distclean
         fi
-        CC=${clamcc} LDFLAGS=${ldflags} CFLAGS=${cflags} ./configure --disable-silent-rules --with-dbdir=/data/clamav/db --disable-clamav --prefix=/data/clamav/install --enable-milter --enable-debug && \
+        CC=${clamcc} LDFLAGS=${ldflags} CFLAGS=${cflags} ./configure --disable-silent-rules --with-dbdir=/data/clamav/db/${dbdir} --disable-clamav --prefix=/data/clamav/install --enable-milter --enable-debug && \
         ${make} -j7 && \
         ${make} check
     ) 2>&1 | tee /tmp/build.log
