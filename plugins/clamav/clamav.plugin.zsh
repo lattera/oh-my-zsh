7d2a2618b2a7f603b5daf092a41928439e0a4227
Shawn Webb <swebb@sourcefire.com>
2014-05-27 12:32:58 -0400

    Enhance the clamav plugin

--- plugins/clamav/clamav.plugin.zsh
+++ plugins/clamav/clamav.plugin.zsh
@@ -4,23 +4,29 @@ if [ "${clam}" = "" ]; then
     clamcc="clang"
 fi
 
-alias sigtool='$(echo ${clam}/sigtool/sigtool) --config=${clamconfig}'
-alias clamscan='$(echo ${clam}/clamscan/clamscan) --config=${clamconfig}'
-alias clamd='$(echo ${clam}/clamd/clamd) --config=${clamconfig}'
-alias clamdscan='$(echo ${clam}/clamdscan/clamdscan) --config=${clamconfig}'
-alias clamdtop='$(echo ${clam}/clamdtop/clamdtop) --config=${clamconfig}'
-alias freshclam='$(echo ${clam}/freshclam/freshclam) --config=${clamconfig}'
-alias dbtool='$(echo ${clam}/dbtool/dbtool) --config=${clamconfig}'
-alias clamconf='$(echo ${clam}/clamconf/clamconf) --config=${clamconfig}'
+alias sigtool='$(echo ${clam}/sigtool/sigtool)'
+alias clamscan='$(echo ${clam}/clamscan/clamscan)'
+alias clamd='$(echo ${clam}/clamd/clamd)'
+alias clamdscan='$(echo ${clam}/clamdscan/clamdscan)'
+alias clamdtop='$(echo ${clam}/clamdtop/clamdtop)'
+alias freshclam='$(echo ${clam}/freshclam/freshclam)'
+alias dbtool='$(echo ${clam}/dbtool/dbtool)'
+alias clamconf='$(echo ${clam}/clamconf/clamconf)'
 
 function buildclam() {
     make="gmake"
     cflags="-g -O2"
     ldflags=""
 
-    if [ $(uname) = "Linux" ]; then
-        make="make"
-    fi
+    case $(uname) in
+        Linux)
+            make="make"
+            ;;
+        FreeBSD)
+            cflags="${cflags} -fPIE"
+            ldflags="${ldflags} -pie"
+            ;;
+    esac
 
     (
         cd $clam
@@ -28,7 +34,6 @@ function buildclam() {
         if [ -f config.status ]; then
             ${make} clean distclean
         fi
-        autoreconf -fi && \
         CC=${clamcc} LDFLAGS=${ldflags} CFLAGS=${cflags} ./configure --disable-silent-rules --with-dbdir=/data/clamav/db --disable-clamav --prefix=/data/clamav/install --enable-milter --enable-debug && \
         ${make} -j7 && \
         ${make} check
